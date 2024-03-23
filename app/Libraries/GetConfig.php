f7013a6108373d8d932b949e7963624b1c593d82
Oleg <108066154+oleg11071981@users.noreply.github.com>
2024-03-23 17:48:22 +0300

    Update GetConfig.php

--- app/Libraries/GetConfig.php
+++ app/Libraries/GetConfig.php
@@ -9,16 +9,16 @@ class GetConfig
 {
     final public static function get_config_data($path, $ad_id = null)
     {
-        if (empty($ad_id)) return config($path); else {
+        if (!empty($ad_id)) {
             $Settings = config('Personal/Settings');
             $ADIDs = $Settings->ADIDs;
             if (in_array($ad_id, $ADIDs)) {
-                $config = config('AdId/' . $ad_id . '/' . $path);
+                $config = config("AdId/$ad_id/$path");
                 if ($config) {
                     return $config;
                 }
             }
-            return config($path);
         }
+        return config($path);
     }
 }
